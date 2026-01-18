63f952c03ca60ddecd50dcde53257671315140f6
Jose Juan Tellez Guzman <josstell@Joses-MacBook-Air.local>
2026-01-18 11:40:57 -0600

    03:Trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import { getQueryClient } from "@/trpc/server";
+import { HydrationBoundary } from "@tanstack/react-query";
 
-export default function Home() {
+export default async  function Home() {
+  const queryClient =  getQueryClient();
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h1 className="text-red-700">Hello</h1>
+      
     </div>
   );
 }
